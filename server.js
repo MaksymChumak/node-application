26e397f4947ed50de2dbf29692cee9ebdbe5ed2b
MaksymChumak <chumak.maksym@gmail.com>
2018-11-14 20:17:31 -0800

    initial commit

---
+++ server.js
@@ -0,0 +1,17 @@
+// server.js
+
+const express = require("express");
+const port = process.env.PORT || 8080;
+
+let app = express();
+
+app.use(express.static(__dirname + "/views"));
+
+app.get('/', (request, response) => {
+  response.render('index.html');
+})
+
+app.listen(port, () => {
+  console.log(`Server is up on port ${port}`);
+});
+
